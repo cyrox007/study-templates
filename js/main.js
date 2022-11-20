110e2d602e701a11b77f3d28ca23c1d5cd83abc7
cyrox007 <cyrox007@gmail.com>
2022-11-20 20:57:21 +0300

    upgrade template

--- js/main.js
+++ js/main.js
@@ -1,44 +1,30 @@
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    effect: 'slide',
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'progressbar',
-    },
-    breakpoints: {
-      // when window width is >= 320px
-      319: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 30
-      },
-      // when window width is >= 640px
-      640: {
-        slidesPerView: 3,
-        spaceBetween: 40
-      }
-    },
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+main = {};
+main.data = {
+  mobileBTN: document.getElementById('mobile-btn'),
+  mNav: document.getElementById('m-nav'),
+  closeBTN: document.getElementById('close'),
+};
+
+(function () {
+  main.data.closeBTN.addEventListener('click', ()=>{
+    let nav = main.data.mNav;
+    
+    nav.classList.toggle('active');
+    nav.querySelector('.m-nav__content').classList.toggle('active');
   });
+  main.data.mobileBTN.addEventListener('click', ()=>{
+    let nav = main.data.mNav;
+    
+    nav.classList.toggle('active');
+    nav.querySelector('.m-nav__content').classList.toggle('active');
+
+    let links = nav.querySelector('.navigate').querySelectorAll('a');
+
+    links.forEach(el=>{
+      el.addEventListener('click', ()=>{
+        nav.classList.toggle('active');
+        nav.querySelector('.m-nav__content').classList.toggle('active');
+      });
+    });
+  });
+}());
